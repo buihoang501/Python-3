9c87e19bac95a708196cf9c5bd70e32554d92bf4
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-09 22:24:40 +0700

    string formatting

---
+++ section-3-object-data-structure-basics/18-string-formatting.py
@@ -0,0 +1,10 @@
+print('Hello {}'.format('Hoang'))
+print('Hello {} {}'.format('Hoang', 'Minh'))
+print('Hello {0} {0}'.format('Hoang', 'Minh'))
+print('Hello {1} {0}'.format('Hoang', 'Minh'))
+print('Hello {m} {h}'.format(h='Hoang', m='Minh'))
+name = 'Hoang'
+print(f'Hello {name}')
+number = 23.3335
+print('Number is {n:1.2f}'.format(n=number))
+print('Number is {n:6.2f}'.format(n=number))
---
+++ section-3-object-data-structure-basics/exercise-5.py
@@ -0,0 +1,1 @@
+'Python {}!'.format('rules')
