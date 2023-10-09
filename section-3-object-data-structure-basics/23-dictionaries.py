c3fc6374fbbc57993a7755a737d27bc253d74278
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-09 23:34:21 +0700

    dictionaries

---
+++ section-3-object-data-structure-basics/23-dictionaries.py
@@ -0,0 +1,9 @@
+hoang = {'name': 'Hoang', 'age': 25, 'points': [
+    5, 6, 7], 'reading': {'science': 0, 'comic': 10000}}
+print(hoang['name'])
+hoang['name'] = 'Minh Hoang'
+hoang['name'].upper()
+print(hoang['name'])
+print(hoang.keys())
+print(hoang.values())
+print(hoang.items())
