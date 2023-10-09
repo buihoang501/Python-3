7c23c3c0beaea6da646aab2ea5f0519f08e58db0
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-09 23:05:27 +0700

    lists

---
+++ section-3-object-data-structure-basics/21-lists.py
@@ -0,0 +1,15 @@
+numbers_a = [1, 2, 3]
+numbers_b = [4, 5, 6]
+print(numbers_a[2])
+numbers_c = numbers_a+numbers_b
+print(numbers_c)
+print(numbers_a[1:])
+numbers_a.pop()
+print(numbers_a)
+numbers_a.append(4)
+print(numbers_a)
+none = numbers_a.sort()
+print(type(none))
+print(numbers_a)
+numbers_a.reverse()
+print(numbers_a)
