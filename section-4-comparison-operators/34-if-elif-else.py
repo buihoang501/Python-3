709f073f58da49a44618b16a40f7a22f08f90340
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-10 11:22:31 +0700

    if-elif-else statements

---
+++ section-4-comparison-operators/34-if-elif-else.py
@@ -0,0 +1,11 @@
+if True:
+    print('Exactly')
+else:
+    print('Incorrectly')
+name = 'Hoang'
+if (name == 'Hoang'):
+    print('Correctly')
+elif (name == 'Minh'):
+    print('A part of correct')
+else:
+    print('Try again!')
