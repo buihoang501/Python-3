b6b44025de4da105d9b5c2fdf2a85211a91d9ae2
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-12 01:14:25 +0700

    test code with pylint library

---
+++ section-10-errors-exception-handling/pylint_84.py
@@ -0,0 +1,16 @@
+# install  pip install pylint
+# run test pylint filename.py -r y
+# remember cd to location which contains your file!
+a = 0
+b = 10
+print(B)
+print(a)
+print(f)
+
+
+def myfunc():
+    ' '
+
+    ' '
+    print(a)
+    print(c)
