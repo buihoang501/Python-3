80b2bee7eaf77fe530f930cd741ccce65573b1e2
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-11 23:43:15 +0700

    __name__ imported and directly run script

---
+++ section-9-modules-and-pakages/79-name-import/one.py
@@ -0,0 +1,10 @@
+def hello_one():
+    print('Hello')
+
+
+hello_one()
+
+if __name__ == '__main__':
+    print('Directly one')
+else:
+    print('Imported one')
---
+++ section-9-modules-and-pakages/79-name-import/two.py
@@ -0,0 +1,10 @@
+import one
+one.hello_one()
+
+if __name__ == '__main__':
+    print('Directly two')
+else:
+    print('Imported two')
+
+# one is imported
+# two rún directly
