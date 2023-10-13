906fac74c071dc991b2488d975a3c5e00a527fba
Hoang Bui <minhhoangbuingo501@gmail.com>
2023-10-13 13:06:02 +0700

    advanced numbers

---
+++ section-20-advanced-objects-datastructure/numbers.py
@@ -0,0 +1,23 @@
+# Converting  hex decimal numbers
+print(hex(5))
+print(hex(16))
+print(hex(12))
+print(hex(15))
+
+# Converting binary
+print(bin(256))
+print(bin(54))
+
+# Pow function for  **
+# (2*2*2*2)%3  2 lũy thừa 4 chia dư cho 3
+number_pow = pow(2, 4, 3)
+print(number_pow)
+
+# absolute number  abs
+print(abs(-209))
+print(abs(22))
+
+
+# round a number
+print(round(2.4))
+print(round(2.6))
